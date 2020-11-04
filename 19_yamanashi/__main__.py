34f9533954e9b721f939b5295328f750d1b70f5d
Tomachi (ICHIGO) <lego@aol.jp>
2020-11-04 21:07:05 +0900

    fix: 山梨県の処理にミスがあったので修正

--- 19_yamanashi/__main__.py
+++ 19_yamanashi/__main__.py
@@ -1,3 +1,6 @@
 # coding: utf-8
 
-print("Another repository: https://github.com/book000/gotoeat-map_yamanashi")
+def main():
+    print("Another repository: https://github.com/book000/gotoeat-map_yamanashi")
+
+main()
